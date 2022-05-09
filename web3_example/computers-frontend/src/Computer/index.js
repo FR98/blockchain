6089f621f862312de5d492459b3c8a759cb428a1
FranciscoRosal <ros18676@uvg.edu.gt>
2022-05-08 22:35:08 -0600

    computer improvements

--- web3_example/computers-frontend/src/Computer/index.js
+++ web3_example/computers-frontend/src/Computer/index.js
@@ -1,11 +1,19 @@
 import './styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Computer() {
+function Computer({ contract, id }) {
+    let available = contract.methods.retrieve_computer_is_available(id).call().then(console.log);
+
+    const useComputer = async () => {
+        await contract.methods.use_computer(id).call();
+    }
+
     return (
         <div className="Computer">
             <FontAwesomeIcon icon="computer" />
-            <p>1</p>
+            <button onClick={() => useComputer()}>
+                <span>{available ? 'Use' : 'Not Available'}</span>
+            </button>
         </div>
     );
 }
