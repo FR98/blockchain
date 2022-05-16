990255f3a5a93b9ac5052c003c7a1ccca6501090
FranciscoRosal <ros18676@uvg.edu.gt>
2022-05-15 19:15:08 -0600

    avances

--- web3_example/computers-frontend/src/components/App/index.js
+++ web3_example/computers-frontend/src/components/App/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import Web3 from 'web3';
+import { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../../store';
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -11,56 +12,57 @@ library.add(faComputer);
 const { store, persistor } = configureStore();
 
 let web3 = null;
-let myAccount = null;
 let computer = null;
 const computer_abi = require('../../blockchain/build/contracts/Computer.json').abi;
-
 
 if (typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask) {
     console.log('Using MetaMask!');
 
     web3 = new Web3(window.ethereum);
     web3.eth.getAccounts().then(console.log);
-    myAccount = window.ethereum.selectedAddress;
+    window.ethereum.request({ method: 'eth_requestAccounts' })
 
     computer = new web3.eth.Contract(computer_abi);
-    computer.options.address = '0x2A1c873b303242618406978F6FAf7241e02ab44f';
+    computer.options.address = '0xef7a6EC8F522E468976cA7eCA5b2AC52f7638F4d'; // Address of the contract
 } else {
     console.log('Using localhost!');
 
     web3 = new Web3("ws://localhost:7545");
     web3.eth.getAccounts().then(console.log);
-    myAccount = '0xBcbE5811817Ce9c7146Ac875535D7195DecE7Eb5';
+    // account = '0xBcbE5811817Ce9c7146Ac875535D7195DecE7Eb5';
 
     computer = new web3.eth.Contract(computer_abi);
-    computer.options.address = '0x2A1c873b303242618406978F6FAf7241e02ab44f';
+    computer.options.address = '0xef7a6EC8F522E468976cA7eCA5b2AC52f7638F4d'; // Address of the contract
 }
 
 function App() {
+
     return (
         <div className="App">
             <Provider store={store}>
                 <header className="App-header">
                     <h1>Computers</h1>
-                    <button onClick={() => {
+                    {/* <button onClick={() => {
+                        web3 = new Web3(window.ethereum);
+                        web3.eth.getAccounts().then(console.log);
                         window.ethereum.request({ method: 'eth_requestAccounts' })
-                    }}>Enable Ethereum</button>
+                    }}>Enable Ethereum</button> */}
                     {
                         computer && (
                             <table>
                                 <tr>
-                                    <td><Computer contract={ computer } id={ 1 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 2 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 3 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 4 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 5 } account={ myAccount } /></td>
+                                    <td><Computer contract={ computer } id={ 1 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 2 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 3 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 4 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 5 } account={ window.ethereum.selectedAddress } /></td>
                                 </tr>
                                 <tr>
-                                    <td><Computer contract={ computer } id={ 6 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 7 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 8 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 9 } account={ myAccount } /></td>
-                                    <td><Computer contract={ computer } id={ 10 } account={ myAccount } /></td>
+                                    <td><Computer contract={ computer } id={ 6 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 7 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 8 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 9 } account={ window.ethereum.selectedAddress } /></td>
+                                    <td><Computer contract={ computer } id={ 10 } account={ window.ethereum.selectedAddress } /></td>
                                 </tr>
                             </table>
                         )
